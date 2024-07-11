3d72c2f3efbc11353e04c0f1eb77297a0cb660e1
fayazkhan <fayazkhan35119@gmail.com>
2024-07-11 15:36:03 +0500

    alert-msg-created

--- src/form/SupplierCRUDform.jsx
+++ src/form/SupplierCRUDform.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+
 function SupplierCRUDform() {
+  const [alert, setAlert] = useState(false);
   const formik = useFormik({
     initialValues: {
       supplier_Category: "",
@@ -21,6 +23,7 @@ function SupplierCRUDform() {
     }),
     onSubmit: async (values) => {
       await axios.post("http://localhost:5000/supplier/post", values);
+      setAlert(!alert);
     },
   });
   return (
@@ -63,6 +66,30 @@ function SupplierCRUDform() {
             </button>
           </form>
         </div>
+
+        {alert ? (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">Added!</strong>
+            <span className="block sm:inline">The record has been added.</span>
+            <span
+              onClick={() => setAlert(!alert)}
+              className="absolute top-0 bottom-0 right-0 px-4 py-3"
+            >
+              <svg
+                className="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
+        ) : null}
       </div>
     </div>
   );
